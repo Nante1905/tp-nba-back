ece0bde2a08a7701ea2178ac5c1889dd3aee21b7
Nante <nantemino15@gmail.com>
2023-11-21 14:50:00 +0300

    feat: [CRUD] crud for entities

---
+++ src/main/java/com/nante/commerce/services/match/EquipeService.java
@@ -0,0 +1,11 @@
+package com.nante.commerce.services.match;
+
+import org.springframework.stereotype.Service;
+
+import com.nante.commerce.crud.service.GenericService;
+import com.nante.commerce.model.match.Equipe;
+
+@Service
+public class EquipeService extends GenericService<Equipe> {
+
+}
